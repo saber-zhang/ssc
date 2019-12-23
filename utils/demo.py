b397e9e82614076c2128a16a3e01f50567cbe8df
saber-zhang <643683438@qq.com>
2019-12-24 00:26:44 +0800

    在demo文件中新增了demo视图

--- utils/demo.py
+++ utils/demo.py
@@ -1,2 +1,11 @@
+from django.http import HttpResponse
+
+
 def index(request):
     print('hahahah')
+    return HttpResponse()
+
+
+def demo(request):
+    print('我是在lancer新增的demo视图')0
+    return HttpResponse()
